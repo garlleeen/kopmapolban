22630f8fe3cfc5bc8246a9c6f8ee37f6a1ee7aff
brada1604 <bagusnugroho1604@gmail.com>
2022-12-15 11:23:27 +0700

    FIX : add sound in scanner

--- resources/views/master/transaksi/index.blade.php
+++ resources/views/master/transaksi/index.blade.php
@@ -6,6 +6,8 @@
     <x-slot name="script">
         <script>
             function onScanSuccess(decodedText, decodedResult) {
+                var audio = new Audio(asset('media/beep.mp3'));
+                audio.play();
                 $("#product_code").val(decodedText);
                 alert("QR berhasil di scan");
             }
